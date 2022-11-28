1bdcdcc770b240a71ecdda4aaa02239f9bf273c5
yasRF <yas070788@hotmail.es>
2022-11-28 20:46:47 +0000

    primer commit

--- src/main.py
+++ src/main.py
@@ -8,8 +8,64 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People
 #from models import Person
+user = [{
+    "id":"0",
+    "name":"Pedro",
+    "dni":"222222222",},
+
+{   "id":"1",
+    "name":"Maria",
+    "dni":"111111111",},
+{
+    "id":"2",
+    "name":"Carlos",
+    "dni":"333333333",},]
+
+favoritos = [ {
+"id":"0",
+"name":"Pedro",
+"name": "R2-D2",
+"name": "Alderaan",
+},
+
+{
+ "id":"1",
+ "name":"Maria",
+ "name": "Leia Organa",
+ "name": "Endor",  
+
+},
+{"id":"2",
+"name":"Carlos",
+"name": "Beru Whitesun lars",
+"name": "Alderaan",},]
+
+
+people = [{
+    "id":"0",
+    "name": "R2-D2",
+	"height": "96",},
+
+    {
+    "id":"1",
+    "name": "Leia Organa",
+	"height": "150",},
+
+    {
+    "id":"2",
+    "name": "Beru Whitesun lars",
+	"height": "165",},]
+
+planet = [{
+    "id":"0",
+    "name": "Alderaan",
+},
+{
+    "id":"1",
+    "name": "Endor",
+	}]
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -31,13 +87,156 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def user_list():
+    user = User.query.all()
+    result = []  
+    for user in user:
+        result.append(user.serialize())
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "lista"
+    
     }
 
-    return jsonify(response_body), 200
+    return jsonify(result), 200
+
+
+
+
+
+
+
+
+@app.route('/user/favoritos', methods=['GET'])
+def userFav():
+     response_body = {
+     "msg": "lista favoritos",
+     "results": favoritos
+ }
+
+     return jsonify(response_body), 200
+  
+
+@app.route('/people', methods=['GET'])
+def people_list():
+    people = People.query.all()
+    result = []  
+    for people in people:
+        result.append(people.serialize())       
+
+    response_body = {
+        "msg": "lista"
+    
+    }
+
+    return jsonify(result), 200
+
+
+@app.route('/people/<int:id>', methods=['GET'])
+def one_person(id):
+    people = People.query.get(1)
+    people = People.query.filter_by(id=1).one()
+    people = People.serialize()
+    response_body = {
+    "get people"}
+   
+
+
+    return jsonify(result), 200
+
+@app.route('/planets', methods=['GET'])
+def planet_list():
+    planets = Planet.query.all()
+    result = []  
+    for planet in planets:
+        result.append(planet.serialize())         
+
+    response_body = {
+        "msg": "lista"
+    
+    }
+
+    return jsonify(result), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def one_planet(id):
+    planet = Planet.query.get(1)
+    planet = Planet.query.filter_by(id=1).one()
+    planet = planet.serialize()
+    response_body = {
+        "get planet"
+    }
+
+
+    return jsonify(result), 200
+
+@app.route('/people', methods=['POST'])
+def add_people():
+   data = request.data
+   data = json.loads(data)
+
+   people = people(name = data["name"],id = data["id"])
+   db.session.add(people)
+   db.session.commit()
+   response_body = {
+      "msg": "Añadiendo personaje"
+
+   },
+   return jsonify(result), 200
+
+
+@app.route('/people', methods=['DELETE'])
+def del_people():
+   data = request.data
+   data = json.loads(data)
+
+   people = people(name = data["name"],id = data["id"])
+   db.session.delete(people)
+   db.session.commit()
+   
+   
+   response_body = {
+      "msg": "Borrando personaje"
+
+   },
+   return jsonify(people), 200
+
+
+@app.route('/planet', methods=['POST'])
+def add_planet():
+   data = request.data
+   data = json.loads(data)
+
+   planet = planet(name = data["name"],id = data["id"])
+   db.session.add(planet)
+   db.session.commit()
+   response_body = {
+      "msg": "Añadiendo planeta"
+
+   },
+   return jsonify(planet), 200
+
+
+@app.route('/planet', methods=['DELETE'])
+def del_planet():
+   data = request.data
+   data = json.loads(data)
+
+   planet = planet(name = data["name"],id = data["id"])
+   db.session.delete(planet)
+   db.session.commit()
+  
+   response_body = {
+      "msg": "borrando planeta"
+
+   },
+   return jsonify(planet), 200
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,50 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    dni = db.Column(db.Integer, unique=False, nullable=False)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+       return self.name #user1
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+    
+            # do not serialize the password, its a security breach
+        }
+
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return self.name #people1
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    def __repr__(self):
+        return self.name #Planeta1
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+    
             # do not serialize the password, its a security breach
         }
